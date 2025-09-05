fb4cb881332cb94f8b403ccaf5eea5743bf08c12
Sahil Rawat <sr4328914@gmail.com>
2025-09-05 14:58:36 +0530

    deploy10

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
       password: data.password,
     };
     await axios
-      .post(`${window.location.origin}/user/login`, userInfo)
+      .post(`https://bookstore-sr.vercel.app/user/login`, userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {

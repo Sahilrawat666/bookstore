4b7811bf82abd5882e961f67a7a50b26b3c99930
Sahil Rawat <sr4328914@gmail.com>
2025-09-11 14:39:14 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -4,32 +4,37 @@ import mongoose from "mongoose";
 import bookRoute from "./route/book.route.js";
 import cors from "cors";
 import userRoute from "./route/user.route.js";
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import { dirname } from "path";
+// import book from "./model/book.model.js";
+// const __filename =  fileURLToPath(import.meta.url);
+
+// const __dirname = dirname(__filename);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 dotenv.config();
 
-const app = express();
-
-//  CORS setup
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
-
-app.use(express.json());
-
-const PORT = process.env.PORT || 4000;
+// const PORT = process.env.PORT || 4000;
 const URI = process.env.MONGODB_URI;
 
-// connect to mongodb
-mongoose
-  .connect(URI)
-  .then(() => console.log("✅ Connected to MongoDB"))
-  .catch((err) => console.error(" MongoDB connection error:", err));
+//connect to mongodb
+try {
+  mongoose.connect(URI);
+  //if using momgodb compass
+  // mongoose.connect(URI, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // });
+  console.log("connected to mongodb");
+} catch (error) {
+  console.log("error:", error);
+}
 
-// routes
+//defining routes
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
 
@@ -37,6 +42,8 @@ app.get("/", (req, res) => {
   res.redirect(process.env.FRONTEND_URL);
 });
 
-app.listen(PORT, () => {
-  console.log(` Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Example app listening on port ${PORT}`);
+// });
+
+export default app;
